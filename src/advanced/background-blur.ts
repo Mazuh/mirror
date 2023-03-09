ec572cc244dc73ce9cf0a26c7a55b563ab571308
mazuh <marcell-mz@hotmail.com>
2023-03-09 07:35:41 -0300

    better explanations

--- src/advanced/background-blur.ts
+++ src/advanced/background-blur.ts
@@ -1,14 +1,18 @@
 import './rvfc-polyfill';
 import pkg from '../../package.json';
 import { CleanupFn } from '../webrtc';
-import { SelfieSegmentation } from '@mediapipe/selfie_segmentation';
+import { Results, SelfieSegmentation } from '@mediapipe/selfie_segmentation';
 
 export async function startBackgrondBlur(
   sourceVideoEl: HTMLVideoElement,
   targetVideoEl: HTMLVideoElement
 ): Promise<CleanupFn> {
-  // setup semantic segmentation and download its trainings
+  // some dom-based stuff
+  const [sourceVideoTrack] = (sourceVideoEl.srcObject as MediaStream).getVideoTracks();
+  const width = sourceVideoEl.clientWidth;
+  const height = sourceVideoEl.clientHeight;
 
+  // setup semantic segmentation, downloading its models and other metadata
   const selfieSegmentation = new SelfieSegmentation({
     locateFile: (file) =>
       `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@${pkg.dependencies['@mediapipe/selfie_segmentation']}/${file}`,
@@ -18,74 +22,78 @@ export async function startBackgrondBlur(
     selfieMode: true,
   });
 
-  // setup drawing instructions
-
-  const canvas = new OffscreenCanvas(sourceVideoEl.clientWidth, sourceVideoEl.clientHeight);
+  // setup drawing callback,
+  // to make use of mediapipe results by blurring each frame in a canvas.
+  const canvas = new OffscreenCanvas(width, height);
   const canvasCtx = canvas.getContext('2d') as OffscreenCanvasRenderingContext2D;
 
-  selfieSegmentation.onResults((results) => {
+  selfieSegmentation.onResults((results: Results) => {
     canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+    canvasCtx.clearRect(0, 0, width, height);
     canvasCtx.filter = '';
 
     // just redraw entire image in default composition.
     canvasCtx.globalCompositeOperation = 'source-over';
-    canvasCtx.drawImage(results.image, 0, 0, canvas.width, canvas.height);
+    canvasCtx.drawImage(results.image, 0, 0, width, height);
 
     // existing image is kept only where selfie segment overlaps it,
     // i.e., now only selfie will remain.
     canvasCtx.globalCompositeOperation = 'destination-in';
-    canvasCtx.drawImage(results.segmentationMask, 0, 0, canvas.width, canvas.height);
+    canvasCtx.drawImage(results.segmentationMask, 0, 0, width, height);
 
     // draw blurred image behind existing content,
     // i.e., now selfie will remain but with the blurred image behind it.
     canvasCtx.filter = 'blur(10px)';
     canvasCtx.globalCompositeOperation = 'destination-over';
-    canvasCtx.drawImage(results.image, 0, 0, canvas.width, canvas.height);
+    canvasCtx.drawImage(results.image, 0, 0, width, height);
 
     canvasCtx.restore();
   });
 
-  // setup processor
-
-  const [sourceVideoTrack] = (sourceVideoEl.srcObject as MediaStream).getVideoTracks();
+  // setup source track reader
   const trackProcessor = new MediaStreamTrackProcessor({ track: sourceVideoTrack });
+  const { readable: trackProcessorReadable } = trackProcessor;
 
+  // setup target track writer
+  const trackGenerator = new MediaStreamTrackGenerator({ kind: 'video' });
+  const { writable: trackGeneratorWritable } = trackGenerator;
+
+  // setup transformer middleware,
+  // it's the bridge between source track and target track.
   const transformer = new TransformStream({
-    async transform(videoFrame, controller) {
-      // send frame for MediaPipe to process (and later generate a result to be handled)
-      videoFrame.width = videoFrame.displayWidth;
-      videoFrame.height = videoFrame.displayHeight;
-      await selfieSegmentation.send({ image: videoFrame });
-      videoFrame.close();
+    transform: async (sourceVideoFrame, controller) => {
+      // feeds mediapipe with source frames
+      // (and mediapipe is in parallel updating the canvas with that callback)
+      // (to be clear, that canvas callback is doing the actual "heavy work")
+      sourceVideoFrame.width = sourceVideoFrame.displayWidth;
+      sourceVideoFrame.height = sourceVideoFrame.displayHeight;
+      await selfieSegmentation.send({ image: sourceVideoFrame });
+      sourceVideoFrame.close();
 
-      // initialize a new frame from canvas and put it on transformation queue
-      const nextFrame = new VideoFrame(canvas, {
-        timestamp: videoFrame.timestamp,
+      // gets whatever is happening on canvas and feed target generator
+      const newFrame = new VideoFrame(canvas, {
+        timestamp: sourceVideoFrame.timestamp,
       });
-      controller.enqueue(nextFrame);
+      controller.enqueue(newFrame);
     },
   });
 
-  // connecting everything...
-  // from source track reader thru transformer and ends at a given track generator,
-  // and such track generator is just a lazy evaluated track to be added to a stream,
-  // and such stream will be played in a regular DOM element.
+  // "connect origin and destination with a bridge"
+  trackProcessorReadable.pipeThrough(transformer).pipeTo(trackGeneratorWritable);
 
-  const trackGenerator = new MediaStreamTrackGenerator({ kind: 'video' });
-  trackProcessor.readable.pipeThrough(transformer).pipeTo(trackGenerator.writable);
-
+  // generating track itself isn't enough and must be inside a stream
   const processedStream = new MediaStream();
   processedStream.addTrack(trackGenerator);
 
+  // now the target stream is attached to a DOM element and starts
   targetVideoEl.srcObject = processedStream;
   await targetVideoEl.play();
 
   // build function to cleanup these resources when needed
-
   const stopSelfieSegmentation = async () => {
-    selfieSegmentation.close(); // fyi, see: https://github.com/google/mediapipe/issues/3373
     processedStream.getTracks().forEach((t) => t.stop());
+    await selfieSegmentation.close(); // fyi, see: https://github.com/google/mediapipe/issues/3373
   };
   return stopSelfieSegmentation;
 }
